03c6b67114ac0ceffb39416c80d1ce364b947c09
Steve Kinney <hello@stevekinney.net>
2014-11-03 20:38:59 -0700

    Define a partial and use it

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ require 'bundler'
 Bundler.require
 
 class FeelGoodBot < Sinatra::Base
+  helpers do
+    def yell(name)
+      name.upcase
+    end
+  end
+  
   get '/' do
     @adjective = ["nice", "alright", "decent"].shuffle.first
     erb :index
