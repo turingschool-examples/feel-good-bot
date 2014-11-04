08dfe480dac0b191b4dbede070042d3e7187d57e
Steve Kinney <hello@stevekinney.net>
2014-11-03 19:51:28 -0700

    Use inline template

--- app.rb
+++ app.rb
@@ -2,13 +2,18 @@ require 'bundler'
 Bundler.require
 
 class FeelGoodBot < Sinatra::Base
+  enable :inline_templates
+
   get '/' do
     @adjective = ["nice", "alright", "decent"].shuffle.first
-    erb %Q{
-      <h1>Good morning!</h1>
-      <p>You look <%= @adjective %> today.</p>
-    }
+    erb :index
   end
 
   run! if app_file == $0
 end
+
+__END__
+
+@@ index
+<h1>Good morning!</h1>
+<p>You look <%= @adjective %> today.</p>
